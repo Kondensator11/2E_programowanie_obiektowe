d82c534312e1471f98a93dbd7ab5874da898b7c5
Kondensator11 <kareko78@wp.pl>
2023-10-10 14:29:19 +0200

    zadanie 1 i zadanie 2 zrobione

---
+++ 10.10.2023/zadanie1.cpp
@@ -0,0 +1,22 @@
+﻿#include <string>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string login, haslo;
+    cout << "podaj login " << endl;
+    cin >> login;
+    cout << "podaj haslo " << endl;
+    cin >> haslo;
+    if (login == "admin" && haslo == "haslo")
+    {
+        cout << "dobrze jesteś juz zalogowany " << endl;
+    }
+    else
+    {
+        cout << "niestety nie zostales zalogowany " << endl;
+    }
+    return 0;
+}
+
---
+++ 10.10.2023/zadanie2.cpp
@@ -0,0 +1,38 @@
+﻿#include <string>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string login, haslo;
+   do 
+   {
+        cout << "podaj login " << endl;
+        cin >> login;
+        cout << "podaj haslo " << endl;
+        cin >> haslo;
+        if (login == "admin" && haslo == "haslo") {
+
+            cout << "dobrze jestes juz zalogowany " << endl;
+            break;
+        }
+
+        else if (login == "admin1" && haslo == "haslo1") {
+
+            cout << "niestety nie zostales zalogowany " << endl;
+            break;
+        }
+        else if (login == "admin2" && haslo == "haslo2") {
+
+            cout << " dobrze jests juz zalogowany";
+            break;
+        }
+        else
+
+            cout << "niestety nie zostales zalogowany" << endl;
+            
+   } while (true);
+
+    return 0;
+}
+
