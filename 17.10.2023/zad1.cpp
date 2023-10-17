a11925e183aca14df15db4c95184ad6808760538
Kondensator11 <kareko78@wp.pl>
2023-10-17 15:02:55 +0200

    zad 1 , i zad 2 zrobione

---
+++ 17.10.2023/zad1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "Podaj pierwsza liczbe parzysta" << endl;
+    cin >> a;
+    if (x != 0)
+    {
+        break;
+    }
+    cout << "Podaj druga liczbe parzysta" << endl;
+    cin >> b;
+
+
+}
