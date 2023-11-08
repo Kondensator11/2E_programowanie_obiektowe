e233dbbe372a05b42f528417ba79fd2f61a3dde3
Kondensator11 <145668893+Kondensator11@users.noreply.github.com>
2023-11-08 10:09:11 +0100

    Update zadanie1.cpp

--- 05.10.2023/zadanie1.cpp
+++ 05.10.2023/zadanie1.cpp
@@ -1,19 +1,4 @@
+następnym razem się wyloguj\
 #include <iostream>
 #include <cstdlib>
 
-using namespace std;
-
-int main()
-{
-    int liczba;
-
-    cout << "wprowadz liczbe" << endl;
-    cin >> liczba;
-
-    if (liczba%2==0) 
-        cout << "wpisana liczba jest parzysta" << endl;
-    else
-        cout << "wpisana liczba jest nieparzysta" << endl;
-
-    return(0);
-}
